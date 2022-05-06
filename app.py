62f7f488f3c521d6ee3d4ffa0c95bfb00b0569c6
Enid Sierra <enidsierra860@outlook.com>
2022-05-06 17:50:30 -0600

    Front v.1

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from datetime import date
 from pprint import pprint
-from flask import Flask, jsonify, request
+from datetime import date, timedelta
+from flask import Flask, jsonify, request, redirect, url_for, render_template
+
 import requests
 import json
 
@@ -8,11 +10,41 @@ app = Flask(__name__)
 
 from images import pictures
 
+#Index route
+@app.route('/', methods=['GET'])
+def ping():
+    #fillDataField("general")
+    print(type(pictures))
+    response_APOC = requests.get('http://127.0.0.1:4000/pictures/general') #probar
+    #traer desde index, page num y asignarlo en el request, validar si este es vacio, 
+    #o si viene con algo hacer con un boton summit get a esta ruta
+    data_response = response_APOC.text
+    pictures_content = json.loads(data_response)
+    print(type(pictures_content))
+    print(type(pictures_content[1]['pictures']))
+   
+
+    for each in pictures_content[1]['pictures']:
+      print(each['title'])
+  
+    print(pictures_content[0]['info']['prev'])
+
+    return render_template("index.html", content = pictures_content) 
+    
+    return jsonify({'General Info': 'http://127.0.0.1:4000/pictures/general'
+    ,'explanation': 'http://127.0.0.1:4000/pictures/explanation'
+    ,'hdurl': 'http://127.0.0.1:4000/pictures/hdurl'
+    ,'title': 'http://127.0.0.1:4000/pictures/title'
+    ,'url': 'http://127.0.0.1:4000/pictures/url'
+    })
+
 #Fills local data with NASA API
 def fillDataField(field):
     global pictures
     pictures.clear()
-    response_API = requests.get('https://api.nasa.gov/planetary/apod?api_key=5D28BHJaqlW7u9okOxk1bFAu6CAhOGPT3629bpBH&start_date=2022-04-29')
+    today = date.today().isoformat()
+    sub_today = str(date.today() - timedelta(days=36))
+    response_API = requests.get(f'https://api.nasa.gov/planetary/apod?api_key=5D28BHJaqlW7u9okOxk1bFAu6CAhOGPT3629bpBH&start_date={sub_today}&end_date={today}')
     #Link should be: https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=100
     data = response_API.text
     pictures_nasa = json.loads(data)
@@ -41,33 +73,26 @@ def fillDataField(field):
     
     return jsonify({'response': 'Response successful'})
 
-#Index route
-@app.route('/', methods=['GET'])
-def ping():
-    return jsonify({'General Info': 'http://127.0.0.1:4000/pictures/general'
-    ,'explanation': 'http://127.0.0.1:4000/pictures/explanation'
-    ,'hdurl': 'http://127.0.0.1:4000/pictures/hdurl'
-    ,'title': 'http://127.0.0.1:4000/pictures/title'
-    ,'url': 'http://127.0.0.1:4000/pictures/url'
-    })
-
-#Pictures routes  
+#Pictures routes *API*
 @app.route('/pictures/<string:field>', methods=['GET'])
 def searchField(field):
+    #prev_num = 0
+    #next_num = 0
     global pictures
     args = request.args
     page_num = args.get('page_num',default=1, type=int)
     page_size = args.get('page_size',default=10, type=int)
     date_supplied = args.get('date',type=str)
 
-    #Load data
-    fillDataField(field)
-
+    if len(pictures) < 1 :
+      fillDataField(field)
+    
     #If date is supplied we generate a filtered list   
     start = (page_num - 1) * page_size
     end = start + page_size
     count = len(pictures)
     filtered_pictures = []
+   
     
     if date_supplied != None:
       for each in pictures:
@@ -75,23 +100,56 @@ def searchField(field):
           filtered_pictures.append(each)
         pictures = filtered_pictures 
 
-    #If entry point is supplied we filter
+    #Defining header
     if end>=count: #last page
       if page_num > 1:
         prev = f"/pictures/{field}?page_num={page_num-1}&page_size={page_size}" 
+        prev_num = page_num - 1
       else: #first page
-        prev = 'None'   
+        prev = 'None'  
+        prev_num = 0 
       next = 'None'
+      next_num = 0
     else: #not last page
       if page_num > 1:
         prev = f"/pictures/{field}?page_num={page_num-1}&page_size={page_size}" 
+        prev_num = page_num - 1
       else: #first page
         prev = 'None'  
+        prev_num = 0
       next = f"/pictures/{field}?page_num={page_num+1}&page_size={page_size}" 
-    
-    general_Info = {'count':count, 'page_num':page_num, 'page_size':page_size, 'next': next, 'prev': prev}
+      next_num = page_num + 1
+    general_Info = {'count':count, 'page_num':page_num, 'page_size':page_size, 'next': next, 'next_page':next_num, 'prev': prev, 'prev_page':prev_num}
 
     return jsonify({'info':general_Info},{'pictures': pictures[start:end]})
+
+#Next or previous route to address
+@app.route('/apod/<string:page_num>', methods=['GET'])
+def paginate(page_num):
+    #fillDataField("general")
+    print(type(pictures))
+    response_APOC = requests.get(f'http://127.0.0.1:4000/pictures/general?page_num={page_num}') #probar
+    #traer desde index, page num y asignarlo en el request, validar si este es vacio, 
+    #o si viene con algo hacer con un boton summit get a esta ruta
+    data_response = response_APOC.text
+    pictures_content = json.loads(data_response)
+    print(type(pictures_content))
+    print(type(pictures_content[1]['pictures']))
+   
+
+    for each in pictures_content[1]['pictures']:
+      print(each['title'])
+  
+    print(pictures_content[0]['info']['prev'])
+
+    return render_template("index.html", content = pictures_content) 
+    
+    return jsonify({'General Info': 'http://127.0.0.1:4000/pictures/general'
+    ,'explanation': 'http://127.0.0.1:4000/pictures/explanation'
+    ,'hdurl': 'http://127.0.0.1:4000/pictures/hdurl'
+    ,'title': 'http://127.0.0.1:4000/pictures/title'
+    ,'url': 'http://127.0.0.1:4000/pictures/url'
+    })
 
 
 if __name__ == '__main__':
