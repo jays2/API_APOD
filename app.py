00596bbac96ccfa4e4d3a2b03a175d99f29ef2b3
Enid Sierra <enidsierra860@outlook.com>
2022-05-07 14:16:46 -0600

    Front v.4

--- app.py
+++ app.py
@@ -12,40 +12,19 @@ from images import pictures
 
 #Index route
 @app.route('/', methods=['GET'])
-def ping():
-    #fillDataField("general")
-    print(type(pictures))
-    response_APOC = requests.get('http://127.0.0.1:4000/pictures/general') #probar
-    #traer desde index, page num y asignarlo en el request, validar si este es vacio, 
-    #o si viene con algo hacer con un boton summit get a esta ruta
+def home():
+    response_APOC = requests.get('http://127.0.0.1:4000/pictures/general')  
     data_response = response_APOC.text
     pictures_content = json.loads(data_response)
-    print(type(pictures_content))
-    print(type(pictures_content[1]['pictures']))
-   
-
-    for each in pictures_content[1]['pictures']:
-      print(each['title'])
-  
-    print(pictures_content[0]['info']['prev'])
-
     return render_template("index.html", content = pictures_content) 
     
-    return jsonify({'General Info': 'http://127.0.0.1:4000/pictures/general'
-    ,'explanation': 'http://127.0.0.1:4000/pictures/explanation'
-    ,'hdurl': 'http://127.0.0.1:4000/pictures/hdurl'
-    ,'title': 'http://127.0.0.1:4000/pictures/title'
-    ,'url': 'http://127.0.0.1:4000/pictures/url'
-    })
-
 #Fills local data with NASA API
 def fillDataField(field):
     global pictures
     pictures.clear()
     today = date.today().isoformat()
-    sub_today = str(date.today() - timedelta(days=15))
+    sub_today = str(date.today() - timedelta(days=16))
     response_API = requests.get(f'https://api.nasa.gov/planetary/apod?api_key=5D28BHJaqlW7u9okOxk1bFAu6CAhOGPT3629bpBH&start_date={sub_today}&end_date={today}')
-    #Link should be: https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=100
     data = response_API.text
     pictures_nasa = json.loads(data)
 
@@ -76,30 +55,18 @@ def fillDataField(field):
 #Pictures routes *API*
 @app.route('/pictures/<string:field>', methods=['GET'])
 def searchField(field):
-    #prev_num = 0
-    #next_num = 0
     global pictures
     args = request.args
     page_num = args.get('page_num',default=1, type=int)
     page_size = args.get('page_size',default=10, type=int)
     date_supplied = args.get('date',type=str)
-
-    
     fillDataField(field)
   
-    #If date is supplied we generate a filtered list   
     start = (page_num - 1) * page_size
     end = start + page_size
-    count = len(pictures)
     filtered_pictures = []
-   
-    
-    print("ASI VIENE EN API")
-    print(date_supplied)
-  
-
+     
     if date_supplied != None:
-      print("ENTRO AL NONEE")
       for each in pictures:
         if each['date'] == date_supplied:
           filtered_pictures.append(each)
@@ -107,8 +74,13 @@ def searchField(field):
 
     count = len(pictures)
 
-    #Defining header
-    if end>=count: #last page
+    #There's only one date per day
+    if date_supplied != None:
+      prev = "None" 
+      prev_num = 0
+      next = "None"
+      next_num = 0
+    elif end>=count: #last page
       if page_num > 1:
         prev = f"/pictures/{field}?page_num={page_num-1}&page_size={page_size}" 
         prev_num = page_num - 1
@@ -126,87 +98,36 @@ def searchField(field):
         prev_num = 0
       next = f"/pictures/{field}?page_num={page_num+1}&page_size={page_size}" 
       next_num = page_num + 1
+    
     general_Info = {'count':count, 'page_num':page_num, 'page_size':page_size, 'next': next, 'next_page':next_num, 'prev': prev, 'prev_page':prev_num}
 
     return jsonify({'info':general_Info},{'pictures': pictures[start:end]})
 
-#Next or previous route to address
+#Next or previous route to address 
 @app.route('/apod/<string:page_num>', methods=['GET'])
 def paginate(page_num):
-    #fillDataField("general")
-
-    #if date is supplied we look by it
-
-    print(type(pictures))
-    response_APOC = requests.get(f'http://127.0.0.1:4000/pictures/general?page_num={page_num}') #probar
-    #traer desde index, page num y asignarlo en el request, validar si este es vacio, 
-    #o si viene con algo hacer con un boton summit get a esta ruta
+    response_APOC = requests.get(f'http://127.0.0.1:4000/pictures/general?page_num={page_num}') 
     data_response = response_APOC.text
     pictures_content = json.loads(data_response)
-    print(type(pictures_content))
-    print(type(pictures_content[1]['pictures']))
    
-
-    for each in pictures_content[1]['pictures']:
-      print(each['title'])
-  
-    print(pictures_content[0]['info']['prev'])
-
     return render_template("index.html", content = pictures_content) 
     
-    return jsonify({'General Info': 'http://127.0.0.1:4000/pictures/general'
-    ,'explanation': 'http://127.0.0.1:4000/pictures/explanation'
-    ,'hdurl': 'http://127.0.0.1:4000/pictures/hdurl'
-    ,'title': 'http://127.0.0.1:4000/pictures/title'
-    ,'url': 'http://127.0.0.1:4000/pictures/url'
-    })
-
 
 #Look for a specific date
 @app.route('/apod/look', methods=['GET'])
 def look():
-    #fillDataField("general")
-
-    #if date is supplied we look by it
     args = request.args
     date_supplied = args.get('fdate',type=str)
-
-    print("FECHA VACIA DESDE EL FORM")
-    print(date_supplied)
-    if date_supplied == None:
-      print("Viene como NONE")
-    
+  
     if date_supplied == "":
       response_APOC = requests.get(f'http://127.0.0.1:4000/pictures/general')  
     else:
       response_APOC = requests.get(f'http://127.0.0.1:4000/pictures/general?date={date_supplied}') 
 
-    print(type(pictures))
-    
-    #traer desde index, page num y asignarlo en el request, validar si este es vacio, 
-    #o si viene con algo hacer con un boton summit get a esta ruta
     data_response = response_APOC.text
     pictures_content = json.loads(data_response)
-    print(type(pictures_content))
-    print(type(pictures_content[1]['pictures']))
-   
-
-    for each in pictures_content[1]['pictures']:
-      print(each['title'])
-  
-    print(pictures_content[0]['info']['prev'])
-
-
-    print("HUBO REQUEST")
 
     return render_template("index.html", content = pictures_content) 
-    
-    return jsonify({'General Info': 'http://127.0.0.1:4000/pictures/general'
-    ,'explanation': 'http://127.0.0.1:4000/pictures/explanation'
-    ,'hdurl': 'http://127.0.0.1:4000/pictures/hdurl'
-    ,'title': 'http://127.0.0.1:4000/pictures/title'
-    ,'url': 'http://127.0.0.1:4000/pictures/url'
-    })
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
